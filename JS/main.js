d5a4dbcde25b28d8d26b3ac5a42412986233a335
Arsssazonov <138324753+Arsssazonov@users.noreply.github.com>
2024-05-27 20:29:34 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+var mobileMenu = document.getElementById("mobilemenu");
+mobileMenu.style.display = "none";
+console.log("JS is on");
+
+function MenuOpen(){
+    
+    console.log("Click!");
+    if (mobileMenu.style.display == "none"){
+        mobileMenu.style.display = "block";
+    }else{
+        mobileMenu.style.display = "none"
+    }
+
+   
+
+
+    
+
+    
+}
